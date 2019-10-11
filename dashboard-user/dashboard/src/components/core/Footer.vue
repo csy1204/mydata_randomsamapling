99487b95b64ab7c44e26c49faedfb06637023951
youngerous <youngerous@gmail.com>
2019-10-11 20:42:16 +0900

    add contents recommendation component

--- dashboard-user/dashboard/src/components/core/Footer.vue
+++ dashboard-user/dashboard/src/components/core/Footer.vue
@@ -4,17 +4,6 @@
     absolute
     height="82"
   >
-    <div class="footer-items">
-      <span
-        v-for="link in links"
-        :key="link.name"
-      >
-        <a
-          :href="link.Link"
-          class="tertiary--text footer-links"
-        >{{ link.name }}</a>
-      </span>
-    </div>
     <v-spacer />
     <span class="font-weight-light copyright">
       &copy;
@@ -36,10 +25,7 @@
   export default {
     data: () => ({
       links: [
-        { name: 'Home', Link: '/#' },
-        { name: 'Creative Tim', Link: 'https://www.creative-tim.com' },
-        { name: 'About Us', Link: 'https://creative-tim.com/presentation' },
-        { name: 'Blog', Link: 'https://blog.creative-tim.com' }
+        
       ]
     })
   }
